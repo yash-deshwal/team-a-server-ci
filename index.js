8c9ad9094e3f6b1ca2794604f476d5f60a71ef85
DanielGiangTran <trantruonggiang7a@gmail.com>
2021-05-13 00:23:42 +1200

    make it better

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+dotenv.config();
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello to Memories API");
+});
+
+// const CONNECTION_URL =
+//   "mongodb+srv://giangtran:giangtran123@clusterdaniel.dknb7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// mongodb+srv://crud:123123123@cluster0.dknb7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set("useFindAndModify", false);
